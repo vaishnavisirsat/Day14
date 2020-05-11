e468cf35a17a35c82391f9e71ece946fd84a1122
“vaishnavi <vaishnavisirsat2717@gmail.com>
2020-05-11 11:13:15 +0530

    enter a valid email

--- Email.sh
+++ Email.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x
-read -p "Enter the postal Index Number :" pin;
-pattern="^abc\.{1}xyz{1}@bl\.{1}co\.{1}in$"
-if [[ $pin =~ $pattern ]]
+read -p "Enter the email id :" email;
+#pattern="^abc\.{1}xyz{1}@bl\.{1}co\.{1}in$"
+pattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-z]{2,4}([.][a-zA-Z]{2,4})$"
+if [[ $email =~ $pattern ]]
 then 
 	echo "valid";
 else
